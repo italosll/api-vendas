10cd04afee30bb8fbf01a6d622f1356b79c9f091
italosll <italomsilva.if@gmail.com>
2021-12-22 18:09:48 -0300

    ✨ Add Celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,18 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm/index';
+import { errors } from 'celebrate';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
