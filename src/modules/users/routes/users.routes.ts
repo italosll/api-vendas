0be30f76845c48ed7f107d27315b3b4416997e32
italosll <italomsilva.if@gmail.com>
2021-12-25 10:05:25 -0300

    ✨ Add User

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import UsersController from '../controllers/UsersController';
+import { celebrate, Joi, Segments } from 'celebrate';
+import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import multer from 'multer';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+
+usersRouter.get('/', isAuthenticated, usersController.index);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+export default usersRouter;
