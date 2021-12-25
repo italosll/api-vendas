af593cac277466826fee8b0320c9c9a0cc4b2480
italosll <italomsilva.if@gmail.com>
2021-12-25 10:07:43 -0300

    ✨ Add Product Controller

--- src/modules/products/controllers/ProductController.ts
+++ src/modules/products/controllers/ProductController.ts
@@ -22,7 +22,7 @@ export default class ProductsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, price, quantity } = request.body;
     const createProduct = new CreateProductService();
-    const product = createProduct.execute({
+    const product = await createProduct.execute({
       name,
       price,
       quantity,
