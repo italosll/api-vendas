fef689f5ce8f250ab93a4737ef2bd96d671df8ad
italosll <italomsilva.if@gmail.com>
2021-12-26 11:31:09 -0300

    ✨ Add profile update

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,9 @@
 import productsRouter from '@modules/products/routes/products.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
-
+import profileRouter from '@modules/users/routes/profile.routes';
 import usersRouter from '@modules/users/routes/users.routes';
+
 import { Router } from 'express';
 
 const routes = Router();
@@ -11,5 +12,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
